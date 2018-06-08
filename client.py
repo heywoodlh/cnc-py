08790712b1a865123b6344ad68fc6719f2acaac5
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-06-08 13:30:22 -0600

    encryption between server and client implemented

--- client.py
+++ client.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
 import socket
+from Crypto.Cipher import AES
 
 host_server = 'localhost'
 host_port = '1776'
+aes_key = '8ZT%a*SJxTD*f6#8C6BfpHmf#DcE5^qH'
+aes_iv = 'sPqapxT*4vZXjZ$w'
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect((host_server, int(host_port)))
-conn_status = 'connection established'
-clientsocket.send(conn_status.encode('utf-8'))
+
+def do_encrypt(message):
+    obj = AES.new(aes_key, AES.MODE_CBC, aes_iv)
+    ciphertext = obj.encrypt(message)
+    return(ciphertext)
+
+def connect_to_server(server, port):
+    global clientsocket
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((server, int(port)))
+    conn_status = 'conn established'
+    conn_status = do_encrypt(conn_status)
+    return(conn_status)
+
+def send_message(clientsocket, message):
+    clientsocket.send(message)
+
+
+def main(server, port):
+    connection_status = connect_to_server(server, port)
+    send_message(clientsocket, connection_status)
+
+
+if __name__ == '__main__':
+    main(host_server, host_port)
--- server.py
+++ server.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python3
 import socket
+from Crypto.Cipher import AES
 
 host_port = '1776'
+aes_key = '8ZT%a*SJxTD*f6#8C6BfpHmf#DcE5^qH'
+aes_iv = 'sPqapxT*4vZXjZ$w'
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('', int(host_port)))
-serversocket.listen(5) # become a server socket, maximum 5 connections
+def server(port):
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind(('', int(host_port)))
+    serversocket.listen(5) # become a server socket, maximum 5 connections
+    return serversocket
 
-while True:
+def do_decrypt(key, iv, ciphertext):
+    obj2 = AES.new(aes_key, AES.MODE_CBC, aes_iv)
+    message = obj2.decrypt(ciphertext)
+    return message
+
+def receive(key, iv, serversocket):
     connection, address = serversocket.accept()
     buf = connection.recv(64)
     if len(buf) > 0:
-        buf = buf.decode('utf-8')
-        print(buf)
+        message = do_decrypt(key, iv, buf)
+        message = message.decode('utf-8')
+        print(message)
+
+
+def main(port, key, iv):
+    serversocket = server(port)
+    while True:
+        receive(key, iv, serversocket)
+
+if __name__ == '__main__':
+    main(host_port, aes_key, aes_iv)
